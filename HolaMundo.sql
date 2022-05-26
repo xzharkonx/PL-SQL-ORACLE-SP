f1d5e3362d5c6b2305de71b6d477ee4445257686
Luis Eduardo Garcia <xzharkonx@gmail.com>
2022-05-26 18:55:32 -0500

    Primer commit Hola mundo

---
+++ HolaMundo.sql
@@ -0,0 +1,17 @@
+-- PL-SQL-ORACLE-SP
+
+-- Esto es necesario para que nosotros podamos ver nuestros dbms_outputs
+set serveroutput on;
+
+-- Inicio
+DECLARE
+-- Definicion de funciones y variables.
+-- Aquí empieza nuestro código.
+BEGIN
+    dbms_output.put_line('Hola Mundo');
+    dbms_output.put_line('Hola Luis');
+ -- Impresión en consola.
+END;
+-- Fin del procedimiento.
+-- Se pone también para indicar el fin del procedimiento. (si marca un error es porque el codigo necesita un cuerpo o algo que hacer).
+/ 
---
+++ SYS.sql
@@ -0,0 +1,3 @@
+--- Para Comprobar si tenemos acceso a la base de datos.
+SELECT * FROM dual;
+alter session set “_ORACLE_SCRIPT”=true;
